3a07133bcda2019169a227d503f9960f658ce859
Yusuf Shaik <100655451@oncampus.local>
2019-03-19 09:53:41 -0400

    first commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main {
+
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your password");
+        String password=in.nextLine();
+        System.out.println(password);
+
+
+    }
+}
